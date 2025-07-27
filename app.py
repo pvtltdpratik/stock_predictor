b1f1a5c52aeb88263e8b93450fdfb51fa11b4970
Ritesh Metange <98689231+RiteshMetange@users.noreply.github.com>
2025-07-27 21:13:44 +0530

    new icon added

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 app.secret_key = 'kolhe'
 
+# Route for homepage
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
